2b8b088615e99f55a055bf197a41525bfb655aad
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-08-28 20:18:34 +0200

    Update run200T.bat

--- Tests/run200T.bat
+++ Tests/run200T.bat
@@ -1,3 +1,4 @@
+::this script was used to run the executable in question for the desired number of times while keeping the C code uncluttered
 @echo off
 for /l %%x in (1, 1, 200) do (
    AlgorithmTest.exe
