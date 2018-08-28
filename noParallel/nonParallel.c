70021b922182aaa8279d504b33866bd15a8355e7
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-08-28 20:35:02 +0200

    Update nonParallel.c

--- noParallel/nonParallel.c
+++ noParallel/nonParallel.c
@@ -8,15 +8,15 @@
 
 int main(){
 
-    int n = 416;
+    int n = 416; //matrices are 416x416 in order to stress test the code and is a multiple of 4
     
 
     int a[n][n];
     int b[n][n];
-    int c[n][n];
-    int i,j,k;
+    int c[n][n]; //declairing arrays
+    int i,j,k; //ideclairing indices
 
-    for (i=0;i<n;i++){
+    for (i=0;i<n;i++){ //initialising arrays
         for (j=0;j<n;j++){
             a[i][j]=rand()%50;
             b[i][j]=rand()%50;
@@ -24,23 +24,24 @@ int main(){
         }
     }
 
-    clock_t begin = clock();
-    //#pragma omp parallel for
-    for (i=0;i<n;i++){
+    clock_t begin = clock(); //begin clock
+    for (i=0;i<n;i++){ //normal serial matrix multiplication
         for (j=0;j<n;j++){
             for (k=0;k<n;k++){
                 c[i][j] += a[i][k]*b[k][j];
             }
         }
     }
-    clock_t end = clock();
+    clock_t end = clock(); //end clock and work out time
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    
     //printf("Time spent: %lf\n",time_spent);
 
-    FILE * fp;
+    FILE * fp; //Writing to the file. The extra comma after the last entry will give us a NaN element in the python array that will be ignored
     fp = fopen ("nopar.txt","a");
     fprintf (fp, "%lf,",time_spent);
     fclose (fp);
+    
     //system("pause");
 
 }
