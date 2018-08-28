b9a94e14212ac561b794f9b3afb80f382b2ad745
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-08-28 20:28:30 +0200

    Update AlgorithmTest.c

--- Tests/AlgorithmTest.c
+++ Tests/AlgorithmTest.c
@@ -7,30 +7,30 @@
 //executable to print the arrays as well for double checking
 
 int main(){
-    srand((int)time(NULL));
+    srand((int)time(NULL)); //randomising the seed a bit using the current time
 
-    int n = 10;
-    int mismatch = 0;
+    int n = 10; //array dimensions
+    int mismatch = 0; //a "boolean" value
 
-    int a[n][n];
-    int b[n][n];
-    int c1[n][n];
-    int c2[n][n];
-    int i,j,k;
+    int a[n][n]; //first matrix
+    int b[n][n]; //second matrix
+    int c1[n][n]; //matrix to hold the serial algorithm's output
+    int c2[n][n]; //matrix to hold the parallel algorithm's output
+    int i,j,k; //indices
 
-    for (i=0;i<n;i++){
+    for (i=0;i<n;i++){ //initialising the arrays
         for (j=0;j<n;j++){
-            a[i][j]=rand()%50;
-            b[i][j]=rand()%50;
-            c1[i][j]=0;
-            c2[i][j]=0;
+            a[i][j]=rand()%50; //a is randomly initialised
+            b[i][j]=rand()%50; //b is randomly initialised
+            c1[i][j]=0; //c1 is a null matrix
+            c2[i][j]=0; //c2 is a null matrix
         }
     }
 
     for (i=0;i<n;i++){
         for (j=0;j<n;j++){
             for (k=0;k<n;k++){
-                c1[i][j] += a[i][k]*b[k][j];
+                c1[i][j] += a[i][k]*b[k][j]; //standard serial matrix multiplication
             }
         }
     }
@@ -40,11 +40,13 @@ int main(){
         #pragma omp parallel for private(k)
         for (j=0;j<n;j++){
             for (k=0;k<n;k++){
-                c2[i][j] += a[i][k]*b[k][j];
+                c2[i][j] += a[i][k]*b[k][j]; //parallelised matrix multiplication
             }
         }
     }
 
+//The code below is comparring c1 to c2 and by uncommenting can also be made to print both arrays
+    
 //    for (i=0;i<n;i++){
 //        for (j=0;j<n;j++){
 //            printf("%d\t",c1[i][j]);
