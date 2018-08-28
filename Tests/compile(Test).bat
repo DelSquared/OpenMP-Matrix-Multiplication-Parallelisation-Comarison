d2eb302693f01db93e2f6e69a4e90742000fe29f
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-08-28 20:16:56 +0200

    Update compile(Test).bat

--- Tests/compile(Test).bat
+++ Tests/compile(Test).bat
@@ -1,3 +1,4 @@
+::This batch script was written to quickly recompile the C code and replace the new one without much hassle
 del /f AlgorithmTest.exe
 gcc -fopenmp AlgorithmTest.c
 ren "a.exe" "AlgorithmTest.exe"
