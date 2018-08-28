98b48693cb8650ad2455604fdd131a237dc199b3
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-08-28 20:15:36 +0200

    Update compile(nonP).bat

--- noParallel/compile(nonP).bat
+++ noParallel/compile(nonP).bat
@@ -1,3 +1,4 @@
+::This batch script was written to quickly recompile the C code and replace the new one without much hassle
 del /f nonParallel.exe
 gcc -fopenmp nonParallel.c
 ren "a.exe" "nonParallel.exe"
