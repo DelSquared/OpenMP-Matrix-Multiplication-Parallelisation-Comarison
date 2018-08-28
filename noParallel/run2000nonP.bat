1852cb822733e7b1eaf1f01cf001b476444a7ac9
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-08-28 20:19:47 +0200

    Update run2000nonP.bat

--- noParallel/run2000nonP.bat
+++ noParallel/run2000nonP.bat
@@ -1,3 +1,4 @@
+::this script was used to run the executable in question for the desired number of times while keeping the C code uncluttered
 @echo off
 for /l %%x in (1, 1, 2000) do (
    nonParallel.exe
