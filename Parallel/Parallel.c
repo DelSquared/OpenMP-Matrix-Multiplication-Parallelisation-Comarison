67212ef9aade0dbe912ea95f0aa0fb3acd101e54
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-08-28 20:34:59 +0200

    Update Parallel.c

--- Parallel/Parallel.c
+++ Parallel/Parallel.c
@@ -8,24 +8,23 @@
 
 int main(){
 
-    int n = 416;
+    int n = 416; //matrices are 416x416 in order to stress test the code and is a multiple of 4
     
 
     int a[n][n];
     int b[n][n];
-    int c[n][n];
-    int i,j,k;
+    int c[n][n]; //declairing arrays
+    int i,j,k; //ideclairing indices
 
-    for (i=0;i<n;i++){
+    for (i=0;i<n;i++){ //initialising arrays
         for (j=0;j<n;j++){
             a[i][j]=rand()%50;
             b[i][j]=rand()%50;
             c[i][j]=0;
         }
     }
-
-    clock_t begin = clock();
-    #pragma omp parallel for private(k)
+    clock_t begin = clock(); //begin clock
+    #pragma omp parallel for private(k) //making sure k is not parallelised
     for (i=0;i<n;i++){
         #pragma omp parallel for private(k)
         for (j=0;j<n;j++){
@@ -34,14 +33,16 @@ int main(){
             }
         }
     }
-    clock_t end = clock();
+    clock_t end = clock(); //end clock and work out time
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    
     //printf("Time spent: %lf\n",time_spent);
 
-    FILE * fp;
+    FILE * fp; //Writing to the file. The extra comma after the last entry will give us a NaN element in the python array that will be ignored
     fp = fopen ("par.txt","a");
     fprintf (fp, "%lf,",time_spent);
     fclose (fp);
+    
     //system("pause");
 
 }
